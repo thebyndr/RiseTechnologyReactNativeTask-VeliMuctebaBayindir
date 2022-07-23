2a4c9e1ef47e1b69d30dcf0b35cd31f83d47cb37
thebyndr <57766774+thebyndr@users.noreply.github.com>
2022-07-23 14:28:15 +0300

    Çevrilen metinler database kaydedilir
    Çevrilen metinler firebase Realtime Database'ye kaydedilir ve oradan çekilerek ekranda kullanıcıya gösterilir

--- Screens/Home.js
+++ Screens/Home.js
@@ -4,14 +4,14 @@ import React,{useEffect,useState} from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup'
 import Voice from '@react-native-community/voice';
-
+import database from '@react-native-firebase/database';
 const Home=(props)=>{
 
   const [translatedText,setTranslatedText]=useState('-')//Çevrilen Metinin Atıldığı Değişken
   const [Flang,setFlang]=useState('en')//Metin Dili
   const [Llang,setLlang]=useState('tr')//Çevrildiği Dil
-  const [TranstlatedWords,setTranslatedWords]=useState([]) 
-
+  const [mic,setMic]=useState(0)//Mikrofon açık mı kapalı mı
+  const [data,setData]=useState([])//Databaseden gelen verileri atmak için 
   const [pitch, setPitch] = useState('');//Ses Düzeyi Değişkeni
   const [error, setError] = useState('');//Error Değişkeni
   const [end, setEnd] = useState('');//Bitme Değişkeni
@@ -19,7 +19,9 @@ const Home=(props)=>{
   const [results, setResults] = useState([]);//Sonuç Değişkeni
   const [partialResults, setPartialResults] = useState([]);//Sonuç Değişkeni
 
-{/*Hangi Dilin Çevrildiğini Anlamak İçin Diller Bir Değişkene Atıldı */}
+
+    
+    {/*Hangi Dilin Çevrildiğini Anlamak İçin Diller Bir Değişkene Atıldı */}
     const changeLang=()=>{
 
         if(Flang=='en'){
@@ -34,7 +36,7 @@ const Home=(props)=>{
   
     {/* Metin Girildiğinde Libretranslate API kullanılarak çeviri gerçekleştirme*/}
     const libreTranslate=async (values) => {
-       setTranslatedWords(oldArray => [values.MetinGir,...oldArray])
+       translatedWords(values)
     const res = await fetch("https://libretranslate.de/translate", {
             method: "POST",
             body: JSON.stringify({
@@ -48,10 +50,20 @@ const Home=(props)=>{
       setTranslatedText(x.translatedText)
     }
 
+    {/*Aranan Kelimeleri Database'e atma */}
+   const translatedWords= (values)=>{
+  database()
+  .ref('/data2'+"/"+values.MetinGir)
+  .set({
+  word:values.MetinGir,
+  
+})
+    }
 
     {/* Konuşma Bitince Libretranslate API kullanılarak Anlanan Metin Çevrilir */}
      const onSpeechPartialResults = async (e) => {
       setPartialResults(e.value)
+   
       // console.log('onSpeechPartialResults: ', e.value);
       const res = await fetch("https://libretranslate.de/translate", {
         method: "POST",
@@ -63,8 +75,12 @@ const Home=(props)=>{
            
         }),
         headers: { "Content-Type": "application/json" }
+
+        
     });
+    
   
+   
     const x=await res.json()
  
     setTranslatedText(x.translatedText)
@@ -79,14 +95,14 @@ const Home=(props)=>{
 
    {/* Konuşma Bitince Çağrılır*/}
     const onSpeechEnd = (e) => {
-
+setMic(0)
     };
 
    {/* Herhangi Bir Hata Oluşursa Çağrılır*/}
     const onSpeechError = (e) => {
     
       setError(JSON.stringify(e.error));
-      
+     
     };
 
 
@@ -94,22 +110,33 @@ const Home=(props)=>{
     const onSpeechResults = (e) => {
       
       setResults(e.value);
-   
-    };
+      if(e.value[0]=="")
+      {
+        setMic(0)
+      }
+        database()
+        .ref('/data2'+"/"+e.value[0])
+      .set({
+        word:e.value[0],
+        
+      });
+    
+   };
    
   
- {/* Mikrofondan gelen sesin düzeyini öğrenme*/}
+   {/* Mikrofondan gelen sesin düzeyini öğrenme*/}
     const onSpeechVolumeChanged = (e) => {
      
       setPitch(e.value);
      
+   
     };
 
 
  {/* Dinlemeye Başlama */}
     const startRecognizing = async () => {
       
-      console.log(Flang)
+     
       try {
         await Voice.start(Flang);
         setPitch('');
@@ -126,12 +153,37 @@ const Home=(props)=>{
     };
    
     
+ {/*Flatlist Render Item (Databaseden gelen verileri gösterme)*/}
+ const renderItem=({item})=>{
+   
+return(
+<View  style={style.FlatlistRenderItemStyle}>
+<Text style={{color:'white',textTransform:'capitalize',fontSize:15,fontFamily:'Geomanist-Book'}}>{item.key}</Text>
+</View  >
+)
+
+    
  
- 
+   
+}
 
 
 useEffect(()=>{
-    
+ {/*Databasedeki Veriyi Çekme */}
+  database()
+  .ref('/data2')
+  .on('value', snapshot => {
+      let newArray=[]
+      snapshot.forEach((item)=>{
+          let ItemObject=item.val();
+          ItemObject['key']=item.key
+         newArray.push(ItemObject)
+        
+      })
+   setData(newArray)
+   
+  });
+  
   Voice.onSpeechStart = onSpeechStart;
   Voice.onSpeechEnd = onSpeechEnd;
   Voice.onSpeechError = onSpeechError;
@@ -152,7 +204,7 @@ return () => {
     return(
        
 <ScrollView style={style.container}>
-  
+
     <View style={{alignItems:'center',justifyContent:'center',width:'100%'}}>
     <Text style={{color:'#546e7a',textTransform:'capitalize',fontSize:26,fontFamily:'Spantaran'}}>Rise VMB Translator</Text>
 
@@ -192,26 +244,31 @@ Yup.object().shape({
       })
   }
   >
+
+    {/* Metinin Girildiği TextInput */}
 {({values,handleSubmit,errors,handleChange,isValid,isSubmitting})=>(
 
 <View style={style.forminput}>
 
 
-{/* Metin Girilem TextInput */}
+
 <View style={style.inputContainer}>
 <TextInput 
 value={values.MetinGir}
+
 autoCapitalize='none'
 placeholder='Metin Girin'
 multiline={true}
 textAlignVertical={'top'}
 textAlign={'center'}
 placeholderTextColor={'#616161'}
+
 onChangeText={handleChange('MetinGir')}
 style={style.inputMetinGir}
+
+
 />
 
-{(errors.MetinGir)&&<Text style={{color:'#e57373',fontSize:18,fontFamily:'sans-serif-condensed'}}>{errors.MetinGir}</Text>}
         </View>
 
 
@@ -226,13 +283,15 @@ style={style.CevirButon}
 
 
 {/* Mikrofon simgesi ve mikrofondan gelen ses */}
-<TouchableOpacity onPress={()=>startRecognizing()} style={{marginBottom:10,alignItems:'center',justifyContent:'center',backgroundColor:'#b3e5fc',borderRadius:50,width:80,height:80}}>
+<TouchableOpacity onPress={()=>{startRecognizing(),setMic(1)}} style={mic==0?style.micClose:style.micOpen}>
 <Icon  name={Platform.OS === "ios" ? "ios-add" : "mic-outline"}
   color={'black'}
   size={30}
       />
         </TouchableOpacity>
        
+
+       {/*Konuşmadan sonra söylenen metini yazdırma*/}
         <View >
           {partialResults.map((result, index) => {
             return (
@@ -256,12 +315,22 @@ style={style.CevirButon}
   </Formik>
 
  {/* Çevrilen Metini Yazdırma */}
-  <View style={{marginBottom:75,alignSelf:'center',backgroundColor:'#cfd8dc',width:300,height:150,alignItems:'center',justifyContent:'center',borderRadius:15}}>
+  <View style={{marginBottom:25,alignSelf:'center',backgroundColor:'#cfd8dc',width:300,height:150,alignItems:'center',justifyContent:'center',borderRadius:15}}>
 
-  <Text style={{color:'gray',textTransform:'capitalize',fontSize:15,fontFamily:'KdamThmorPro-Regular'}}>{translatedText}</Text>
+  <Text style={{color:'gray',textTransform:'capitalize',fontSize:12,fontFamily:'KdamThmorPro-Regular'}}>{translatedText}</Text>
 
   </View>
- 
+
+   {/* FlatList , daha önce çevrilmiş kelimeleri yazdırma */}
+  <View style={style.FlatlistStyle}>
+       <FlatList 
+       data={data}
+       renderItem={renderItem}
+       numColumns={2}/>
+
+
+   </View>
+
 </ScrollView>
 
     )
@@ -322,7 +391,41 @@ marginHorizontal:15,
 fontWeight:'700'},
 imageButton:{
   width:40,height:40
-}
+},
+FlatlistStyle:{
+  paddingHorizontal:10,
+  marginTop:15
+  
+      },
+      FlatlistRenderItemStyle:{
+        flex:1,
+        marginBottom:15,
+        marginHorizontal:10,
+        paddingVertical:5,
+        justifyContent:'center',
+        alignItems:'center',
+        
+        borderRadius:15,
+        backgroundColor:'#4ebaaa'
+        },
+        micClose:{
+          marginBottom:10,
+          alignItems:'center',
+          justifyContent:'center',
+          backgroundColor:'#b3e5fc',
+          borderRadius:50,
+          width:80,
+          height:80
+        },
+        micOpen:{
+          marginBottom:10,
+          alignItems:'center',
+          justifyContent:'center',
+          backgroundColor:'gray',
+          borderRadius:50,
+          width:80,
+          height:80
+        }
 
 })
 export default Home
